2402e8b8fa1383bd8e99d509932b3b5515daaf2b
chollasit-dev <118034127+chollasit-dev@users.noreply.github.com>
2025-04-01 18:07:44 +0700

    build(vite): set explicit input and output HTML
    Configure Vite to acknowledge other HTML files apart from index.html.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,23 @@
 import tailwindcss from '@tailwindcss/vite';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
+
+let __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        checkout: resolve(__dirname, 'pages/', 'checkout.html'),
+        dashboard: resolve(__dirname, 'pages/', 'dashboard.html'),
+        payment: resolve(__dirname, 'pages/', 'payment.html'),
+        'product-detail': resolve(__dirname, 'pages/', 'product-detail.html'),
+        'sign-in': resolve(__dirname, 'pages/', 'sign-in.html'),
+        'sign-up': resolve(__dirname, 'pages/', 'sign-up.html'),
+      },
+    },
+  },
 });
